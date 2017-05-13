a01a80929b5747b20d4244e4d2c1e2c7a98135d9
mrndjo <mrndjo@gmail.com>
2017-05-13 14:11:49 +0200

    Dodat MainPAge, LoginForma i OdabirLokacije

--- ProjekatRentACar/ProjekatRentACar/Views/MainPage.xaml.cs
+++ ProjekatRentACar/ProjekatRentACar/Views/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ProjekatRentACar.Views;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,12 @@ namespace ProjekatRentACar
         public MainPage()
         {
             this.InitializeComponent();
+            MainFrame.Navigate(typeof(FormaPrijava));
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
         }
     }
 }

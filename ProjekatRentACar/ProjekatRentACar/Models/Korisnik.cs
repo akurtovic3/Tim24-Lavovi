99c630fad7fb0fbd4cdff8968ac572e90ab29ac5
Ehvan Gradanin <egradanin1@etf.unsa.ba>
2017-05-27 17:29:47 +0200

    merge

---
+++ ProjekatRentACar/ProjekatRentACar/Models/Korisnik.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace ProjekatRentACar.Models
+{
+   public  enum Medalje { Zlato, Srebro, Bronza };
+    public class Korisnik:Osoba
+    {
+        
+        private Medalje rejting { get; set; }
+        private List<Najam> najmovi;
+
+        private int id;
+        public Medalje Rejting
+        {
+            get { return rejting; }
+            set { rejting = value; }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public List<Najam> Najmovi
+        {
+            get
+            {
+                return najmovi;
+            }
+
+            set
+            {
+                najmovi = value;
+            }
+        }
+
+        public Korisnik(string ime, string prezime, DateTime datum_rodjenja, string telefon, string email, string adresa,
+           int postanski_broj, string korisnicko_ime, string sifra, Image slika, Medalje r,int i) : base(ime, prezime, datum_rodjenja, telefon, email, adresa, postanski_broj, korisnicko_ime, sifra, slika)
+        {
+            Rejting = r;
+            Id=i;
+            Najmovi = new List<Najam>();
+            
+        }
+        public void iznajmiVozilo(Najam n)
+        {
+            Najmovi.Add(n);
+           
+        }
+    }
+}

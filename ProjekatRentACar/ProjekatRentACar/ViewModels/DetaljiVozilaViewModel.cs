2c0ad369a0e42dac706d450a6e37cff5176d21aa
Senadin Terovic <sterovic1@etf.unsa.ba>
2017-05-29 18:59:56 +0200

    uradjeni detalji vozila

---
+++ ProjekatRentACar/ProjekatRentACar/ViewModels/DetaljiVozilaViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ProjekatRentACar.Helper;
+using ProjekatRentACar.Views;
+using ProjekatRentACar.Models;
+
+namespace ProjekatRentACar.ViewModels
+{
+    class DetaljiVozilaViewModel
+    {
+        public ICommand Dalje { get; set; }
+        NavigationService navigacija;
+        public Vozilo odabranoVozilo { get; set; }
+
+        public DetaljiVozilaViewModel(Vozilo vozilo)
+        {
+            Dalje = new RelayCommand<object>(prikaziOpremu);
+            navigacija = new NavigationService();
+            odabranoVozilo = vozilo;
+        }
+
+        void prikaziOpremu(object parameter)
+        {
+            navigacija.Navigate(typeof(FormaOdabirOpreme));
+        }
+    }
+}

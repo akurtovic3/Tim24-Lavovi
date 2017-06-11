12943a3b8f4775bf0eae371bfe98eb2b07ede76b
mrndjo <mrndjo@gmail.com>
2017-06-11 19:31:29 +0200

    Detalji korisnika

---
+++ ProjekatRentACar/ProjekatRentACar/Models/NajamWebModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatRentACar.Models
+{
+    public class NajamWebModel
+    {
+        public int Id { get; set; }
+        public DateTime PocetniDatum { get; set; }
+        public DateTime KrajnjiDatum { get; set; }
+        public double Cijena { get; set; }
+        public string Proizvodjac { get; set; }
+        public string Model { get; set; }
+        public string Slika { get; set; }
+    }
+}
---
+++ ProjekatRentACar/ProjekatRentACar/Models/NajmoviKorisnikaDataSource.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace ProjekatRentACar.Models
+{
+    public class NajmoviKorisnikaDataSource
+    {
+        private List<NajamWebModel> najmovi
+        {
+            get; set;
+        }
+
+        public List<NajamWebModel> Najmovi
+        {
+            get
+            {
+                return najmovi;
+            }
+            set
+            {
+                najmovi = value;
+            }
+        }
+
+        private string date { get; set; }
+
+        public async Task preuzmiNajmove(int korisnikID, Action callback)
+        {
+            Najmovi = new List<NajamWebModel>();
+            HttpClient httpClient = new HttpClient();
+            string urlString = "http://lavovi.space/api/lista_iznajmljenih_vozila.php?korisnik_id=" + korisnikID.ToString();
+            string response = await httpClient.GetStringAsync(new Uri(urlString));
+            JsonArray value = JsonArray.Parse(response).GetArray();
+            for (uint i = 0; i < value.Count; i++)
+            {
+                NajamWebModel noviNajam = new NajamWebModel();
+                IJsonValue jsonValue;
+                if (value.GetObjectAt(i).TryGetValue("id", out jsonValue))
+                {
+                    noviNajam.Id = Convert.ToInt32(jsonValue.GetString());
+                }
+                if (value.GetObjectAt(i).TryGetValue("pocetni_datum", out jsonValue))
+                {
+                    noviNajam.PocetniDatum =  DateTime.ParseExact(jsonValue.GetString().Replace("\"",""), "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                }
+                if (value.GetObjectAt(i).TryGetValue("krajnji_datum", out jsonValue))
+                {
+                    noviNajam.KrajnjiDatum = DateTime.ParseExact(jsonValue.ToString().Replace("\"", ""), "yyyy-MM-dd",
+                                       System.Globalization.CultureInfo.InvariantCulture);
+                }
+                if (value.GetObjectAt(i).TryGetValue("cijena", out jsonValue))
+                {
+                    noviNajam.Cijena = Convert.ToDouble(jsonValue.GetString());
+                }
+                if (value.GetObjectAt(i).TryGetValue("proizvodjac", out jsonValue))
+                {
+                    noviNajam.Proizvodjac = jsonValue.GetString();
+                }
+                if (value.GetObjectAt(i).TryGetValue("model", out jsonValue))
+                {
+                    noviNajam.Model = jsonValue.GetString();
+                }
+                if (value.GetObjectAt(i).TryGetValue("slika", out jsonValue))
+                {
+                    noviNajam.Slika = jsonValue.GetString();
+                }
+
+                Najmovi.Add(noviNajam);
+            }
+            callback();
+        }
+    }
+
+   
+}

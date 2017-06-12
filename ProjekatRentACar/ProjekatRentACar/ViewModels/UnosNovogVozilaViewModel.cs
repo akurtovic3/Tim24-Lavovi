a9a424ab5cd23c718d007d36ddc13731107af842
Senadin Terovic <sterovic1@etf.unsa.ba>
2017-06-12 17:31:57 +0200

    validiran unos vozila

--- ProjekatRentACar/ProjekatRentACar/ViewModels/UnosNovogVozilaViewModel.cs
+++ ProjekatRentACar/ProjekatRentACar/ViewModels/UnosNovogVozilaViewModel.cs
@@ -15,10 +15,13 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Windows.Storage.Streams;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Prism.Windows.Validation;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjekatRentACar.ViewModels
 {
-    class UnosNovogVozilaViewModel : INotifyPropertyChanged
+    class UnosNovogVozilaViewModel : ValidatableBindableBase, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -32,25 +35,88 @@ namespace ProjekatRentACar.ViewModels
         public ICommand UnesiVozilo { get; set; }
         public ICommand DodajSliku { get; set; }
 
+        public ObservableCollection<string> erori { get; set; }
+
+        private TipVozila tip;
+        [Required]
+        public TipVozila Tip { get { return tip; } set { SetProperty(ref tip, value); } }
+
+        private string model;
+        [Required]
+        public string Model { get { return model; } set { SetProperty(ref model, value); } }
+
+        private string proizvodjac;
+        [Required]
+        public string Proizvodjac { get { return proizvodjac; } set { SetProperty(ref proizvodjac, value); } }
+
+        private VrstaGoriva vrstaGoriva;
+        [Required]
+        public VrstaGoriva VrstaGoriva { get { return vrstaGoriva; } set { SetProperty(ref vrstaGoriva, value); } }
+
+        private string snaga;
+        [Required]
+        public string Snaga { get { return snaga; } set { SetProperty(ref snaga, value); } }
+
+        private string kilometraza;
+        [Required]
+        public string Kilometraza { get { return kilometraza; } set { SetProperty(ref kilometraza, value); } }
+
+        private string brojSjedista;
+        [Required]
+        public string BrojSjedista { get { return brojSjedista; } set { SetProperty(ref brojSjedista, value); } }
+
+        private string brojVrata;
+        [Required]
+        public string BrojVrata { get { return brojVrata; } set { SetProperty(ref brojVrata, value); } }
+
+        private string godiste;
+        [Required]
+        public string Godiste { get { return godiste; } set { SetProperty(ref godiste, value); } }
+
+        private bool klima;
+        [Required]
+        public bool Klima { get { return klima; } set { SetProperty(ref klima, value); } }
+
+        private bool navigacija;
+        [Required]
+        public bool Navigacija { get { return navigacija; } set { SetProperty(ref navigacija, value); } }
+
+        private Mjenjac vrstaMjenjaca;
+        [Required]
+        public Mjenjac VrstaMjenjaca { get { return vrstaMjenjaca; } set { SetProperty(ref vrstaMjenjaca, value); } }
+
+        private int zapreminaPrtljaznika;
+        [Required]
+        public int ZapreminaPrtljaznika { get { return zapreminaPrtljaznika; } set { SetProperty(ref zapreminaPrtljaznika, value); } }
+
+        private string kubikaza;
+        [Required]
+        public string Kubikaza { get { return kubikaza; } set { SetProperty(ref kubikaza, value); } }
+
+        private string popust;
+        [Required]
+        public string Popust { get { return popust; } set { SetProperty(ref popust, value); } }
+
+        private string cijenaPoDanu;
+        [Required]
+        public string CijenaPoDanu { get { return cijenaPoDanu; } set { SetProperty(ref cijenaPoDanu, value); } }
+
+        private double cijenaSaPopustom;
+        [Required]
+        public double CijenaSaPopustom { get { return cijenaSaPopustom; } set { SetProperty(ref cijenaSaPopustom, value); } }
+
         private BitmapImage slika;
+        [Required]
         public BitmapImage Slika
         {
             get { return slika; }
             set
             {
-                slika = value;
+                SetProperty(ref slika, value);
                 OnNotifyPropertyChanged("Slika");
             }
         }
 
-        public string Snaga { get; set; }
-        public string Kilometraza { get; set; }
-        public string BrojSjedista { get; set; }
-        public string BrojVrata { get; set; }
-        public string Godiste { get; set; }
-        public string Kubikaza { get; set; }
-        public string Popust { get; set; }
-        public string CijenaPoDanu { get; set; }
         private UploadVozilaDataSource uploadDS;
 
         private byte[] byteArray;
@@ -63,21 +129,39 @@ namespace ProjekatRentACar.ViewModels
             uploadDS = new UploadVozilaDataSource();
         }
 
-        public  void unosVozila(object parameter)
+        public async void unosVozila(object parameter)
         {
 
             //Treba prvo ispitati validaciju, pa ako prođe validacija, tek onda pokrenuti ovaj kod ispod...
-            /*
-            NovoVozilo.BrojSjedista = Convert.ToInt32(BrojSjedista);
-            NovoVozilo.BrojVrata = Convert.ToInt32(BrojVrata);
-            NovoVozilo.Snaga = Convert.ToInt32(Snaga);
-            NovoVozilo.Kilometraza = Convert.ToInt32(Kilometraza);
-            NovoVozilo.Godiste = Convert.ToInt32(Godiste);
-            NovoVozilo.Popust = Convert.ToInt32(Popust);
-            NovoVozilo.Kubikaza = Convert.ToDouble(Kubikaza);
-            NovoVozilo.CijenaPoDanu = Convert.ToDouble(CijenaPoDanu);
-         */
-            uploadDS.unesiVozilo(novoVozilo, byteArray, callback).GetAwaiter();
+
+            this.ValidateProperties();
+            erori = new ObservableCollection<string>(Errors.Errors.Values.SelectMany(x => x).ToList());
+
+            if ((erori == null || erori.Count == 0))
+            {
+                NovoVozilo.Tip = Tip;
+                NovoVozilo.Proizvodjac = Proizvodjac;
+                NovoVozilo.Model = Model;
+                NovoVozilo.VrstaGoriva = VrstaGoriva;
+                NovoVozilo.BrojSjedista = Convert.ToInt32(BrojSjedista);
+                NovoVozilo.BrojVrata = Convert.ToInt32(BrojVrata);
+                NovoVozilo.VrstaMjenjaca = VrstaMjenjaca;
+                NovoVozilo.Klima = Klima;
+                NovoVozilo.Navigacija = Navigacija;
+                NovoVozilo.ZapreminaPrtljaznika = 4500;
+                NovoVozilo.Snaga = Convert.ToInt32(Snaga);
+                NovoVozilo.Kilometraza = Convert.ToInt32(Kilometraza);
+                NovoVozilo.Godiste = Convert.ToInt32(Godiste);
+                NovoVozilo.Popust = Convert.ToInt32(Popust);
+                NovoVozilo.Kubikaza = Convert.ToDouble(Kubikaza);
+                NovoVozilo.CijenaPoDanu = Convert.ToDouble(CijenaPoDanu);
+                NovoVozilo.CijenaSaPopustom = NovoVozilo.CijenaPoDanu - (NovoVozilo.CijenaPoDanu * NovoVozilo.Popust / 100);
+
+                uploadDS.unesiVozilo(novoVozilo, byteArray, callback).GetAwaiter();
+
+                var msd = new MessageDialog("Uspješno ste unijeli novo vozilo");
+                await msd.ShowAsync();
+            }           
         }
 
 

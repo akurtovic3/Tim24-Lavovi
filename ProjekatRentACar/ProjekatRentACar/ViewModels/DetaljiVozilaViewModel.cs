a3b33567569c479f6800d22b02bc368905eb97b1
Senadin Terovic <sterovic1@etf.unsa.ba>
2017-06-12 18:59:44 +0200

    uradjen binding ikonica u detaljima vozila

--- ProjekatRentACar/ProjekatRentACar/ViewModels/DetaljiVozilaViewModel.cs
+++ ProjekatRentACar/ProjekatRentACar/ViewModels/DetaljiVozilaViewModel.cs
@@ -15,16 +15,28 @@ namespace ProjekatRentACar.ViewModels
         public ICommand Dalje { get; set; }
         NavigationService navigacija;
 
+        public string osoba { get; set; }
+        public char kofer { get; set; }
+        public char mjenjac { get; set; }
+        public string vrata { get; set; }
+
         public Najam najam { get; set; }
         public DetaljiVozilaViewModel(Najam najam)
         {
-            Dalje = new RelayCommand<object>(prikaziOpremu);
+            Dalje = new RelayCommand<object>(prikaziOVozacu);
             navigacija = new NavigationService();
             this.najam = najam;
             this.najam.Cijena = (najam.KrajniDatum - najam.PocetniDatum).TotalDays * najam.Vozilo.CijenaPoDanu;
+
+            //osoba = Convert.ToChar(najam.Vozilo.BrojSjedista);
+            //vrata = Convert.ToChar(najam.Vozilo.BrojVrata);
+            osoba = najam.Vozilo.BrojSjedista.ToString();
+            vrata = najam.Vozilo.BrojVrata.ToString();
+            kofer = (najam.Vozilo.ZapreminaPrtljaznika >= 5000) ? '3' : '2';
+            mjenjac = (najam.Vozilo.VrstaMjenjaca == Mjenjac.Automatski) ? 'A' : 'M'; 
         }
 
-        void prikaziOpremu(object parameter)
+        void prikaziOVozacu(object parameter)
         {
             if (App.daLiJeKorisnikPrijavljen)
             {
